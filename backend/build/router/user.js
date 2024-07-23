a2c3e0905ef7f9bc5b53f14c5cfdb58b21db9049
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-07-23 14:21:13 +0200

    Delete folder build

--- backend/build/router/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routerUser = (0, express_1.Router)();
-routerUser.get('/', (_req, res) => {
-    console.log(process.env.PORT);
-    return res.status(200).json({ msg: 'Hello word' });
-});
-exports.default = routerUser;
